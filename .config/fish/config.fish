6ce24dc00a7830a4ea4622c048860e7b1e015d7a
lcd <206488166+CDanhhh@users.noreply.github.com>
2026-03-22 15:04:01 +0700

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias lzg="lazygit"
+alias lzdk="lazydocker"
+alias yaz="yazi"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
