30e82f974c2a67ca268debaa8a939d3a15943fad
lcd <206488166+CDanhhh@users.noreply.github.com>
2026-03-29 16:28:44 +0700

    add stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,18 @@ alias lzg="lazygit"
 alias lzdk="lazydocker"
 alias yaz="yazi"
 
+zoxide init fish | source
+
+
+set -gx GTK_IM_MODULE fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx XMODIFIERS @im=fcitx
+set -gx SDL_IM_MODULE fcitx
+set -gx GLFW_IM_MODULE ibus
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
+thefuck --alias | source
